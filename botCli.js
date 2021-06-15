18da73fd87b415bc43bc43e3936f57eebf60f653
ramzdevlab <muhamad.fahmi@outlook.com>
2021-06-16 02:14:07 +0700

    Update BOT CLI 4

--- botCli.js
+++ botCli.js
@@ -9,157 +9,294 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+console.log('\nCHOOSE MENU : \n (1) EARLY BUY TARGET \n (2) EARLY BUY TARGET');
 
-rl.question("Token Target ? ", function(target) {
-  
-      const addresses = {
-          WBNB: `${process.env.WBNB}`,
-          TARGET : `${target}`,
-          factory: `${process.env.FACTORY}`, 
-          router: `${process.env.ROUTER}`,
-          recipient: `${process.env.RECEIPENT}`
-      }
+rl.question("\nChoose ? ", function(menu) {
+  if(menu == 1){
+    
+
+    rl.question("\nToken Target ? ", function(target) {
+      //START PROGRAM TARGET 
 
 
-      const privateKey = `${process.env.PRIVATE_KEY}`;
-      const mygasPrice = ethers.utils.parseUnits('5', 'gwei');
-      const provider = new ethers.providers.WebSocketProvider('wss://bsc-ws-node.nariox.org:443');
-      const wallet = new ethers.Wallet(privateKey);
-      const account = wallet.connect(provider);
-
-      const router = new ethers.Contract(
-        addresses.router,
-        [
-          'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
-          'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
-        ],
-        account
-      );
-      const erc = new ethers.Contract(
-          addresses.WBNB,
-          [{"constant": true,"inputs": [{"name": "_owner","type": "address"}],"name": "balanceOf","outputs": [{"name": "balance","type": "uint256"}],"payable": false,"type": "function"}],
-          account
-        );  
-
-        const factory = new ethers.Contract(
-          addresses.factory,
-          [
-            'event PairCreated(address indexed token0, address indexed token1, address pair, uint)',
-            'function getPair(address tokenA, address tokenB) external view returns (address pair)'
-            ],
-          account
-        ); 
-
-      console.log("BOT STARTED !!!")
-
-
-      factory.on('PairCreated', async (token0, token1, pairAddress) => {
-          var date = await dt.format('d-m-Y');
-          var time 
-          time = await dt.format('H:M:S');
-          let tokenIn, tokenOut;
-          if(token0 === addresses.WBNB) {
-            tokenIn = token0;
-            tokenOut = token1;
-          }
+        const addresses = {
+            WBNB: `${process.env.WBNB}`,
+            TARGET : `${target}`,
+            factory: `${process.env.FACTORY}`, 
+            router: `${process.env.ROUTER}`,
+            recipient: `${process.env.RECEIPENT}`
+        }
         
-          if(token1 == addresses.WBNB) {
-            tokenIn = token1;
-            tokenOut = token0;
-          }
-          if(typeof tokenIn === 'undefined') {
-              return;
+        
+        const privateKey = `${process.env.PRIVATE_KEY}`;
+        const mygasPrice = ethers.utils.parseUnits(`${process.env.GWEI}`, 'gwei');
+        const provider = new ethers.providers.WebSocketProvider('wss://bsc-ws-node.nariox.org:443');
+        const wallet = new ethers.Wallet(privateKey);
+        const account = wallet.connect(provider);
+        
+        const router = new ethers.Contract(
+          addresses.router,
+          [
+            'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
+            'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
+          ],
+          account
+        );
+        const erc = new ethers.Contract(
+            addresses.WBNB,
+            [{"constant": true,"inputs": [{"name": "_owner","type": "address"}],"name": "balanceOf","outputs": [{"name": "balance","type": "uint256"}],"payable": false,"type": "function"}],
+            account
+          );  
+        
+          const factory = new ethers.Contract(
+            addresses.factory,
+            [
+              'event PairCreated(address indexed token0, address indexed token1, address pair, uint)',
+              'function getPair(address tokenA, address tokenB) external view returns (address pair)'
+              ],
+            account
+          ); 
+        
+        console.log("BOT STARTED !!!")
+        
+        
+        factory.on('PairCreated', async (token0, token1, pairAddress) => {
+            var date = await dt.format('d-m-Y');
+            var time 
+            time = await dt.format('H:M:S');
+            let tokenIn, tokenOut;
+            if(token0 === addresses.WBNB) {
+              tokenIn = token0;
+              tokenOut = token1;
             }
           
+            if(token1 == addresses.WBNB) {
+              tokenIn = token1;
+              tokenOut = token0;
+            }
+            if(typeof tokenIn === 'undefined') {
+                return;
+              }
             
-          try {
-              const getPairx = await factory.getPair(tokenIn, tokenOut); 
-              const pairBNBvalue = await erc.balanceOf(getPairx);   
-              var bnbne = ethers.utils.formatEther(pairBNBvalue);
-              console.log(`new token => https://bscscan.com/token/${tokenOut} - liquidity ${bnbne} BNB`);
-            
-              if(tokenOut === addresses.TARGET){
-                  console.log('\n\n=========================================================')
-                  console.log('=> TOKEN ', addresses.TARGET, ' -> FOUND')
-                  console.log('(', date, '-', time, `) new token => https://bscscan.com/token/${tokenOut} - liquidity ${bnbne} BNB`);
-                  console.log('=========================================================\n\n')
-                  
-                  
-                  //APPROVE ----------------------------------------------------------------
-                  const wbnb1 = new ethers.Contract(
-                      addresses.WBNB,
-                      [
-                        'function approve(address spender, uint amount) public returns(bool)',
-                      ],
-                      account
-                    );
-                  console.log(`Before Approve`);
-                  const valueToapprove = ethers.utils.parseUnits('0.005', 'ether');
-                  const init = async () => {
-                      const tx = await wbnb1.approve(
-                      router.address, 
-                      valueToapprove,
-                      {
-                          gasPrice: mygasPrice,
-                          gasLimit: 210000
-                      }
+              
+            try {
+                const getPairx = await factory.getPair(tokenIn, tokenOut); 
+                const pairBNBvalue = await erc.balanceOf(getPairx);   
+                var bnbne = ethers.utils.formatEther(pairBNBvalue);
+                console.log(`new token => https://bscscan.com/token/${tokenOut} - liquidity ${bnbne} BNB`);
+              
+                if(time >= tim)
+        
+                if(tokenOut === addresses.TARGET){
+                    console.log('\n\n=========================================================')
+                    console.log('=> TOKEN ', addresses.TARGET, ' -> FOUND')
+                    console.log('(', date, '-', time, `) new token => https://bscscan.com/token/${tokenOut} - liquidity ${bnbne} BNB`);
+                    console.log('=========================================================\n\n')
+                    
+                    
+                    //APPROVE ----------------------------------------------------------------
+                    const wbnb1 = new ethers.Contract(
+                        addresses.WBNB,
+                        [
+                          'function approve(address spender, uint amount) public returns(bool)',
+                        ],
+                        account
                       );
-                      console.log(`After Approve`);
+                    console.log(`Before Approve`);
+                    const valueToapprove = ethers.utils.parseUnits('0.005', 'ether');
+                    const init = async () => {
+                        const tx = await wbnb1.approve(
+                        router.address, 
+                        valueToapprove,
+                        {
+                            gasPrice: mygasPrice,
+                            gasLimit: process.env.GAS_LIMIT
+                        }
+                        );
+                        console.log(`After Approve`);
+                        const receipt = await tx.wait(); 
+                        console.log('Transaction receipt');
+                        console.log(receipt);
+                    } 
+        
+                    init();
+                    
+                    //BUY ----------------------------------------------------------------
+        
+                    const testtx = async () => {
+                    console.log(`after testtx`);
+        
+                    let tokenIn = addresses.WBNB , tokenOut = addresses.TARGET;
+                    
+                    
+                      const amountIn = ethers.utils.parseUnits('0.001', 'ether');
+                      const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut]);
+                      //Our execution price will be a bit different, we need some flexbility
+                      const amountOutMin = amounts[1].sub(amounts[1].div(process.env.AMMOUNT_OUT_MIN));
+        
+                      console.log(`
+                        Buying new token
+                        =================
+                        tokenIn: ${amountIn} ${tokenIn} (WBNB)
+                        tokenOut: ${amountOutMin} ${tokenOut}
+                      `);
+        
+                      const tx = await router.swapExactTokensForTokens(
+                        amountIn,
+                        amountOutMin,
+                        [tokenIn, tokenOut],
+                        addresses.recipient,
+                        Math.floor(Date.now() / 1000) + 60 * 20, // 20 minutes from the current Unix time
+                        {
+                            gasPrice: mygasPrice,
+                            gasLimit: process.env.GAS_LIMIT
+                        }
+                      );
                       const receipt = await tx.wait(); 
                       console.log('Transaction receipt');
                       console.log(receipt);
-                  } 
-
-                  init();
-                  
-                  //BUY ----------------------------------------------------------------
-
-                  const testtx = async () => {
-                  console.log(`after testtx`);
-
-                  let tokenIn = addresses.WBNB , tokenOut = addresses.TARGET;
-                  
-                  
-                    const amountIn = ethers.utils.parseUnits('0.001', 'ether');
-                    const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut]);
-                    //Our execution price will be a bit different, we need some flexbility
-                    const amountOutMin = amounts[1].sub(amounts[1].div(10));
-
-                    console.log(`
-                      Buying new token
-                      =================
-                      tokenIn: ${amountIn} ${tokenIn} (WBNB)
-                      tokenOut: ${amountOutMin} ${tokenOut}
-                    `);
-
-                    const tx = await router.swapExactTokensForTokens(
-                      amountIn,
-                      amountOutMin,
-                      [tokenIn, tokenOut],
-                      addresses.recipient,
-                      Math.floor(Date.now() / 1000) + 60 * 20, // 20 minutes from the current Unix time
-                      {
-                          gasPrice: mygasPrice,
-                          gasLimit: 210000
-                      }
-                    );
-                    const receipt = await tx.wait(); 
-                    console.log('Transaction receipt');
-                    console.log(receipt);
-                  }
-                  testtx();
+                    }
+                    testtx();
+        
+        
+        
+                }
+        
+        
+              
+            }catch(e){
+                console.log(e)
+            }
+        });
 
 
+      //END PROGRAM TARGET 
 
-              }
+      rl.close();
+    });
+    
+
+  }else if(menu == 2){
+    
+    rl.question("\nToken Address ? ", function(token) {
+      
+       // TOKEN BUY TARGET BOT-==================================
+        const addresses = {
+            WBNB: `${process.env.WBNB}`,
+            TARGET : `${token}`,
+            factory: `${process.env.FACTORY}`, 
+            router: `${process.env.ROUTER}`,
+            recipient: `${process.env.RECEIPENT}`
+        }
+        
+        
+        const privateKey = `${process.env.PRIVATE_KEY}`;
+        const mygasPrice = ethers.utils.parseUnits(`${process.env.GWEI}`, 'gwei');
+        const provider = new ethers.providers.WebSocketProvider('wss://bsc-ws-node.nariox.org:443');
+        const wallet = new ethers.Wallet(privateKey);
+        const account = wallet.connect(provider);
+        
+        const router = new ethers.Contract(
+          addresses.router,
+          [
+            'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
+            'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
+          ],
+          account
+        );
+      
+        console.log("BUY BOT STARTED !!!")
+        
+        
+
+        //APPROVE ----------------------------------------------------------------
+        const wbnb1 = new ethers.Contract(
+            addresses.WBNB,
+            [
+              'function approve(address spender, uint amount) public returns(bool)',
+            ],
+            account
+          );
+        console.log(`Before Approve`);
+
+       
+        const valueToapprove = ethers.utils.parseUnits('0.005', 'ether');
+        const init = async () => {
+            const tx = await wbnb1.approve(
+            router.address, 
+            valueToapprove,
+            {
+                gasPrice: mygasPrice,
+                gasLimit: process.env.GAS_LIMIT
+            }
+            );
+            console.log(`After Approve`);
+            const receipt = await tx.wait(); 
+            console.log('Transaction receipt');
+            console.log(receipt);
 
 
-            
-          }catch(e){
-              console.log(e)
-          }
-      });
-    rl.close();
+
+        } 
+
+        init();
+        
+        //BUY ----------------------------------------------------------------
+
+        const testtx = async () => {
+        console.log(`after testtx`);
+
+        let tokenIn = addresses.WBNB , tokenOut = addresses.TARGET;
+        
+        
+          const amountIn = ethers.utils.parseUnits(`${process.env.AMMOUNT_IN}`, 'ether');
+          const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut]);
+          //Our execution price will be a bit different, we need some flexbility
+          const amountOutMin = amounts[1].sub(amounts[1].div(process.env.AMMOUNT_OUT_MIN));
+          
+          console.log(`
+            Buying new token
+            =================
+            tokenIn: ${amountIn} ${tokenIn} (WBNB)
+            tokenOut: ${amountOutMin} ${tokenOut}
+          `);
+
+
+          const tx = await router.swapExactTokensForTokens(
+            amountIn,
+            amountOutMin,
+            [tokenIn, tokenOut],
+            addresses.recipient,
+            Math.floor(Date.now() / 1000) + 60 * 20, // 20 minutes from the current Unix time
+            {
+                gasPrice: mygasPrice,
+                gasLimit: process.env.GAS_LIMIT
+            }
+          );
+          const receipt = await tx.wait(); 
+          console.log('Transaction receipt');
+          console.log(receipt);
+          
+        }
+        testtx();
+      
+
+    // TOKEN BUY TERGET END ===============================
+
+      rl.close();
+    });
+    
+
+  }else{
+    console.log('COMMAND IS INVALID !')
+    process.exit(1);
+  }
+  
+
+  
+  
+  
 });
+
 
 
